25c10a0a87a42a8588634f90430e0a8fa07007d3
onomlight <pass041@nate.com>
2023-02-09 14:51:43 +0900

    중첩경로

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,14 @@
+import { Route } from 'react-router-dom'
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <Route path="/welcome/new-user">
+        <p>뉴뉴저</p>
+      </Route>
+    </section>
+  )
 }
 
 export default Welcome
+// 어느곳이나 정의가 가능하고 react-router-dom에 의해 평가됨
