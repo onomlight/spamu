4f561a714943a4413abd450bd592e391bc22d315
onomlight <pass041@nate.com>
2023-02-12 15:56:42 +0900

    페이지 추가

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,22 @@
 import { Fragment } from 'react'
 import { Route, useParams } from 'react-router-dom'
+
+import HighlightedQuote from '../components/quotes/HighlightedQuote'
 import Comments from '../components/comments/Comments'
+const DUMMY = [
+  { id: 'q1', author: 'max', text: '일초' },
+  { id: 'q2', author: 'max2', text: '2초' },
+]
 const QuoteDetail = () => {
   const params = useParams()
 
+  const quote = DUMMY.find((quote) => quote.id === params.quoteId)
+  if (!quote) {
+    return <h1>No find level!</h1>
+  }
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
